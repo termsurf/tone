dec6cd9ff568dc1663a97cf97668552274e665ad
Lance Pollard <lp@elk.fm>
2021-01-01 11:11:11 -0800

    start

---
+++ language/chinese-pinyin.js
@@ -0,0 +1,544 @@
+
+const m = {
+  'b': 'p',
+  'd': 't',
+  'g': 'k',
+  'p': 'ph',
+  't': 'th',
+  'k': 'kh',
+  'm': 'm',
+  'n': 'n',
+  'zh': 't+x',
+  'z': 'ts',
+  'j': 'tx',
+  'ch': 't+xh',
+  'c': 'tsh',
+  'q': 'txh',
+  'f': 'f',
+  'sh': 'x',
+  's': 's',
+  'x': 'x~',
+  'h': 'h~',
+  'l': 'l',
+  'r': 'r',
+  'y': 'y',
+  'w': 'w',
+
+  'iang': 'yaq',
+  'iāng': 'ya^q',
+  'iàng': 'ya^a_q',
+  'iáng': 'yaa^q',
+  'iǎng': 'ya^a_a^q',
+
+  'iang1': 'ya^q',
+  'iang2': 'ya^a_q',
+  'iang4': 'yaa^q',
+  'iang3': 'ya^a_a^q',
+
+  'iong': 'yo+q',
+  'iǒng': 'yo+^o+_o+^q',
+  'iōng': 'yo+^q',
+  'ióng': 'yo+^q',
+  'iòng': 'yo+^o+_q',
+
+  'iong3': 'yo+^o+_o+^q',
+  'iong1': 'yo+^q',
+  'iong2': 'yo+^q',
+  'iong4': 'yo+^o+_q',
+
+  'ing': 'iq',
+  'íng': 'ii^q',
+  'ìng': 'i^i_q',
+  'īng': 'i^q',
+  'ǐng': 'i^i_i^q',
+
+  'ing2': 'ii^q',
+  'ing4': 'i^i_q',
+  'ing1': 'i^q',
+  'ing3': 'i^i_i^q',
+
+  'iao': 'yau',
+  'iāo': 'ya^u',
+  'iào': 'ya^a_u',
+  'iáo': 'yaa^u',
+  'iǎo': 'ya^a_a^u',
+
+  'iao1': 'ya^u',
+  'iao4': 'ya^a_u',
+  'iao2': 'yaa^u',
+  'iao3': 'ya^a_a^u',
+
+  'ian': 'ya+n',
+  'iān': 'ya+^n',
+  'iǎn': 'ya+^a+_a+^n',
+  'ián': 'ya+^n',
+  'iàn': 'ya+^a+_n',
+
+  'ian1': 'ya+^n',
+  'ian3': 'ya+^a+_a+^n',
+  'ian2': 'ya+^n',
+  'ian4': 'ya+^a+_n',
+
+  'ia': 'ya',
+  'iā': 'ya^',
+  'ià': 'ya_',
+  'iá': 'yaa^',
+  'iǎ': 'ya^a_a^',
+
+  'ia1': 'ya^',
+  'ia4': 'ya_',
+  'ia2': 'yaa^',
+  'ia3': 'ya^a_a^',
+
+  'ie': 'ye',
+  'iē': 'ye^',
+  'iě': 'ye^e_e^',
+  'ié': 'yee^',
+  'iè': 'ye^e_',
+
+  'ie1': 'ye^',
+  'ie3': 'ye^e_e^',
+  'ie2': 'yee^',
+  'ie4': 'ye^e_',
+
+  'iu': 'you',
+  'iǔ': 'you^u_u^',
+  'iū': 'you^',
+  'iú': 'youu^',
+  'iù': 'you^u_',
+
+  'iu3': 'you^u_u^',
+  'iu1': 'you^',
+  'iu2': 'youu^',
+  'iu4': 'you^u_',
+
+  'in': 'in',
+  'ín': 'ii^n',
+  'ìn': 'i^i_n',
+  'īn': 'i^n',
+  'ǐn': 'i^i_i^n',
+
+  'in2': 'ii^n',
+  'in4': 'i^i_n',
+  'in1': 'i^n',
+  'in3': 'i^i_i^n',
+
+  'io': 'yo',
+  'iō': 'yo^',
+  'iò': 'yo^o_',
+  'ió': 'yoo^',
+  'iǒ': 'yo^o_o^',
+
+  'io1': 'yo^',
+  'io4': 'yo^o_',
+  'io2': 'yoo^',
+  'io3': 'yo^o_o^',
+
+  'eng': 'u+q',
+  'ēng': 'u+^q',
+  'ěng': 'u+^u+_u+^q',
+  'éng': 'u+u+^q',
+  'èng': 'u+^u+_q',
+
+  'eng1': 'u+^q',
+  'eng3': 'u+^u+_u+^q',
+  'eng2': 'u+u+^q',
+  'eng4': 'u+^u+_q',
+
+  'ei': 'ei',
+  'ēi': 'e^i',
+  'ěi': 'e^e_e^i',
+  'éi': 'ee^i',
+  'èi': 'e^e_i',
+
+  'ei1': 'e^i',
+  'ei3': 'e^e_e^i',
+  'ei2': 'ee^i',
+  'ei4': 'e^e_i',
+
+  'en': 'u+n',
+  'ēn': 'u+^n',
+  'ěn': 'u+^u+_u+^n',
+  'én': 'u+u+^n',
+  'èn': 'u+^u+_n',
+
+  'en1': 'u+^n',
+  'en3': 'u+^u+_u+^n',
+  'en2': 'u+u+^n',
+  'en4': 'u+^u+_n',
+
+  'er': 'a+o~',
+  'ēr': 'a+^o~',
+  'ěr': 'a+^a+_a+^o~',
+  'ér': 'a+^o~',
+  'èr': 'a+^a+_o~',
+
+  'er1': 'a+^o~',
+  'er3': 'a+^a+_a+^o~',
+  'er2': 'a+^o~',
+  'er4': 'a+^a+_o~',
+
+  'ang': 'aq',
+  'āng': 'a^q',
+  'àng': 'a^a_q',
+  'áng': 'aa^q',
+  'ǎng': 'a^a_a^q',
+
+  'ang1': 'a^q',
+  'ang4': 'a^a_q',
+  'ang2': 'aa^q',
+  'ang3': 'a^a_a^q',
+
+  'ai': 'ai',
+  'āi': 'a^i',
+  'ǎi': 'a^a_a^i',
+  'ái': 'aa^i',
+  'ài': 'a^a_i',
+
+  'ai1': 'a^i',
+  'ai3': 'a^a_a^i',
+  'ai2': 'aa^i',
+  'ai4': 'a^a_i',
+
+  'ao': 'au',
+  'āo': 'a^u',
+  'ào': 'a^a_u',
+  'áo': 'aa^u',
+  'ǎo': 'a^a_a^u',
+
+  'ao1': 'a^u',
+  'ao4': 'a^a_u',
+  'ao2': 'aa^u',
+  'ao3': 'a^a_a^u',
+
+  'an': 'an',
+  'ān': 'a^n',
+  'àn': 'a^a_n',
+  'án': 'aa^n',
+  'ǎn': 'a^a_a^n',
+
+  'an1': 'a^n',
+  'an4': 'a^a_n',
+  'an2': 'aa^n',
+  'an3': 'a^a_a^n',
+
+  'ong': 'o+q',
+  'ōng': 'o+^q',
+  'óng': 'o+^q',
+  'ǒng': 'o+^o+_o+^q',
+  'òng': 'o+^o+_q',
+
+  'ong1': 'o+^q',
+  'ong2': 'o+^q',
+  'ong3': 'o+^o+_o+^q',
+  'ong4': 'o+^o+_q',
+
+  'uang': 'waq',
+  'uāng': 'wa^q',
+  'uàng': 'wa^a_q',
+  'uáng': 'waa^q',
+  'uǎng': 'wa^a_a^q',
+
+  'uang1': 'wa^q',
+  'uang4': 'wa^a_q',
+  'uang2': 'waa^q',
+  'uang3': 'wa^a_a^q',
+
+  'uai': 'wai',
+  'uāi': 'wa^i',
+  'uài': 'wa^a_i',
+  'uái': 'waa^i',
+  'uǎi': 'wa^a_a^i',
+
+  'uai1': 'wa^i',
+  'uai4': 'wa^a_i',
+  'uai2': 'waa^i',
+  'uai3': 'wa^a_a^i',
+
+  'uan': 'wan',
+  'uān': 'wa^n',
+  'uàn': 'wa^a_n',
+  'uán': 'waa^n',
+  'uǎn': 'wa^a_a^n',
+
+  'uan1': 'wa^n',
+  'uan4': 'wa^a_n',
+  'uan2': 'waa^n',
+  'uan3': 'wa^a_a^n',
+
+  'uan': 'ṳa+n',
+  'uān': 'ṳa+^n',
+  'uǎn': 'ṳa+^a+_a+^n',
+  'uán': 'ṳa+^n',
+  'uàn': 'ṳa+^a+_n',
+
+  'uan1': 'ṳa+^n',
+  'uan3': 'ṳa+^a+_a+^n',
+  'uan2': 'ṳa+^n',
+  'uan4': 'ṳa+^a+_n',
+
+  'ua': 'wu+',
+  'uā': 'wu+^',
+  'uǎ': 'wu+^u+_u+^',
+  'uá': 'wu+u+^',
+  'uà': 'wu+^u+_',
+
+  'ua1': 'wu+^',
+  'ua3': 'wu+^u+_u+^',
+  'ua2': 'wu+u+^',
+  'ua4': 'wu+^u+_',
+
+  'uo': 'wo',
+  'uō': 'wo^',
+  'uó': 'woo^',
+  'uǒ': 'wo^o_o^',
+  'uò': 'wo^o_',
+
+  'uo1': 'wo^',
+  'uo2': 'woo^',
+  'uo3': 'wo^o_o^',
+  'uo4': 'wo^o_',
+
+  'ui': 'wei',
+  'uí': 'weii^',
+  'uì': 'wei^i_',
+  'uī': 'wei^',
+  'uǐ': 'wei^i_i^',
+
+  'ui2': 'weii^',
+  'ui4': 'wei^i_',
+  'ui1': 'wei^',
+  'ui3': 'wei^i_i^',
+
+  'un': 'wu+n',
+  'ǔn': 'wu+^u+_u+^n',
+  'ūn': 'wu+^n',
+  'ún': 'wu+u+^n',
+  'ùn': 'wu+^u+_n',
+
+  'un3': 'wu+^u+_u+^n',
+  'un1': 'wu+^n',
+  'un2': 'wu+u+^n',
+  'un4': 'wu+^u+_n',
+
+  'ue': 'ṳe',
+  'uē': 'ṳe^',
+  'uě': 'ṳe^e_e^',
+  'ué': 'ṳee^',
+  'uè': 'ṳe^e_',
+
+  'ue1': 'ṳe^',
+  'ue3': 'ṳe^e_e^',
+  'ue2': 'ṳee^',
+  'ue4': 'ṳe^e_',
+
+  'un': 'yn',
+  'ǔn': 'yu+^u+_u+^n',
+  'ūn': 'yu+^n',
+  'ún': 'yu+u+^n',
+  'ùn': 'yu+^u+_n',
+
+  'un3': 'yu+^u+_u+^n',
+  'un1': 'yu+^n',
+  'un2': 'yu+u+^n',
+  'un4': 'yu+^u+_n',
+
+  'ou': 'ou',
+  'ōu': 'o^u',
+  'óu': 'oo^u',
+  'ǒu': 'o^o_o^u',
+  'òu': 'o^o_u',
+
+  'ou1': 'o^u',
+  'ou2': 'oo^u',
+  'ou3': 'o^o_o^u',
+  'ou4': 'o^o_u',
+
+  'i': 'i',
+  'í': 'ii^',
+  'i2': 'ii^',
+  'ì': 'i^i_',
+  'i4': 'i^i_',
+  'ī': 'i^',
+  'i1': 'i^',
+  'ǐ': 'i^i_i^',
+  'i3': 'i^i_i^',
+  'ê': 'a+',
+  'ē': 'o+^',
+  'e1': 'o+^',
+  'ě': 'o+^o+_o+^',
+  'e3': 'o+^o+_o+^',
+  'é': 'o+^',
+  'e2': 'o+^',
+  'è': 'o+^o+_',
+  'e4': 'o+^o+_',
+  'e': 'o+',
+  'ā': 'a^',
+  'a1': 'a^',
+  'à': 'a^a_',
+  'a4': 'a^a_',
+  'á': 'aa^',
+  'a2': 'aa^',
+  'ǎ': 'a^a_a^',
+  'a3': 'a^a_a^',
+  'a': 'a',
+  'ō': 'wo^',
+  'o1': 'wo^',
+  'ó': 'woo^',
+  'o2': 'woo^',
+  'ǒ': 'wo^o_o^',
+  'o3': 'wo^o_o^',
+  'ò': 'wo^o_',
+  'o4': 'wo^o_',
+  'o': 'wo',
+  'o': 'o',
+  'ǔ': 'u^u_u^',
+  'u3': 'u^u_u^',
+  'ū': 'u^',
+  'u1': 'u^',
+  'ú': 'uu^',
+  'u2': 'uu^',
+  'ù': 'u^u_',
+  'u4': 'u^u_',
+  'u': 'u',
+
+  'yun': 'ywun',
+  'yǔn': 'ywu^u_u^n',
+  'yūn': 'ywu^n',
+  'yún': 'ywuu^n',
+  'yùn': 'ywu^u_n',
+
+  'yun3': 'ywu^u_u^n',
+  'yun1': 'ywu^n',
+  'yun2': 'ywuu^n',
+  'yun4': 'ywu^u_n',
+
+  'yu': 'ywu',
+  'yǔ': 'ywu^u_u^',
+  'yū': 'ywu^',
+  'yú': 'ywuu^',
+  'yù': 'ywu^u_',
+
+  'yu3': 'ywu^u_u^',
+  'yu1': 'ywu^',
+  'yu2': 'ywuu^',
+  'yu4': 'ywu^u_',
+
+  ' ': ' ',
+  '　': ' ',
+  '，': ', ',
+  ',': ',',
+  '。': '. ',
+  '.': '.',
+  '？': '? ',
+  '?': '?',
+  '；': '; ',
+  ';': ';',
+  '：': ': ',
+  ':': ':',
+  '（': ' (',
+  '(': '(',
+  '）': ') ',
+  ')': ')',
+  '［': ' [',
+  '[': '[',
+  '］': '] ',
+  ']': ']',
+  '【': ' [',
+  '】': '] ',
+  '『': '"',
+  '』': '"',
+  '「': "'",
+  '」': "'",
+  '"': '"',
+  '"': '"',
+  "'": "'",
+  "'": "'",
+  '“': '"',
+  '”': '"',
+  '‘': "'",
+  '’': "'",
+  '、': ', ',
+  '\n': '\n',
+  '‧': ' ',
+  '‧': ' ',
+  '《': ' <',
+  '》': '> ',
+  '〈': ' <',
+  '〉': '> ',
+  '<': '<',
+  '>': '>',
+  '…': '...',
+  '⋯': '...',
+  '～': '~',
+  '—': '-',
+  ' ': ' '
+}
+
+const s = build(m)
+
+const form = t => transform(t, s, m)
+
+module.exports = form
+
+/**
+ * Transform input text to output using map.
+ */
+
+function transform(i, s, m) {
+  let o = []
+  let w = 0
+
+  while (w < i.length) {
+    let r = s
+    let v = w
+
+    while (true) {
+      let c = i.charAt(v).toLowerCase()
+      if (r[c]) {
+        r = r[c]
+        v++
+      } else {
+        break
+      }
+    }
+
+    if (r === s) {
+      const t = i.charAt(w).toLowerCase()
+      const h = t.codePointAt(0).toString(16)
+      const e = '\\u' + '0000'.substring(0, 4 - h.length) + h
+      throw new Error(`${w}:${e}:${t}`)
+    }
+
+    let z = i.substr(w, v - w).toLowerCase()
+
+    if (typeof m[z] == 'function') {
+      m[z](o)
+    } else {
+      o.push(m[z])
+    }
+
+    w = v
+  }
+
+  return o.join('')
+}
+
+/**
+ * Build trie.
+ */
+
+function build(m) {
+  let s = {}
+
+  for (let key in m) {
+    let v = key.toLowerCase().split('')
+    let r = s
+    while (v.length) {
+      var x = v.shift()
+      r = r[x] = r[x] || {}
+    }
+  }
+
+  return s
+}
---
+++ readme.md
@@ -0,0 +1,196 @@
+
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+
+<p align='center'>
+  <img src='https://github.com/mountbuild/hanakana.js/blob/build/title.png?raw=true' width='500'/>
+  <img src='https://github.com/mountbuild/hanakana.js/blob/build/description.png?raw=true' width='500'/>
+</p>
+
+<br/>
+
+<p align='center'>
+  <a href='#summary'>Summary</a>　　
+  <a href='#usage'>Usage</a>　　
+  <a href='#dictionaries'>Dictionaries</a>　　
+  <a href='#dictionaries'>Symbols</a>　　
+  <a href='#license'>License</a>
+</p>
+
+<br/>
+<br/>
+<br/>
+
+<h3 id="summary">Summary</h3>
+
+Hanákana is a Universal Writing System (UWS), capable of representing all human voice sounds in a concise and readable script. This means you can write any language in Hanákana script (constructed language or natural language) and read it without having to learn that language's specific quirks. Hanákana is a sound-based script like the [Ancient Runes](https://en.wikipedia.org/wiki/Runes), which means when you read see a symbol/letter, you know exactly how it is pronounced. There are no weird edge cases or specific words for which you have to memorize the sound. You simply learn the script and can pronounce the words and texts. You can find how to pronounce the letters in the [PDF guide](https://github.com/mountbuild/hanakana.js/blob/build/guide.pdf) or partly on [leaf.surf](https://leaf.surf).
+
+You may note that Linguistics also has the International Phonetic Alphabet ([IPA](https://en.wikipedia.org/wiki/International_Phonetic_Alphabet)), which seems to accomplish a similar thing. But the IPA is not for writing anything but the most detailed pronunciation guides, not for writing sentences or long texts. Hanákana is meant for the later, for writing like you would in a normal language.
+
+The ASCII encoding is mapped to IPA for your knowledge at [ovo.js](https://github.com/mountbuild/ovo.js).
+
+Ideally Hanákana would be included in the Unicode standard. But [that is unlikely](http://www.unicode.org/pending/proposals.html) until it is used widely. We have suggested it be used as a writing system [on Mars](https://twitter.com/mountbuild/status/1372723474741927938) 😂.
+
+<p align="center">
+  <img src="https://github.com/mountbuild/hanakana.js/blob/build/transform.gif?raw=true" height="300" />
+</p>
+
+These are the 32 main sounds. Everything else are variations of these sounds.
+
+```
+hawk a
+game e
+need i
+home o
+tune u
+
+last a
+mess e
+tick i
+luck u
+book o
+
+moon m
+note n
+ring ng
+game g
+dock d
+boat b
+team t
+pick p
+king k
+hook h
+seek s
+fish f
+vote v
+zone z
+yell y
+wind w
+thud th
+ship sh
+rule r
+loom l
+
+then th
+joke j (sure as in measure)
+```
+
+```
+variation on m
+sanskrit,अहिंसा,ahiṃsā,nonviolence
+
+variation on sh
+sanskrit,उपनिषद्,upaniṣad
+
+variation on sh
+sanskrit,शिव,śiva
+
+variation on r
+sanskrit,कृष्ण,kṛṣṇa
+
+variation on t
+sanskrit,हठ,haṭha
+
+variation on n
+sanskrit,पुराण,purāṇa
+
+variation on d
+Sanskrit,ढोल,ḍhola
+
+variation on h
+arabic,حديث‎,ḥadīṯ
+
+variation on h
+arabic,شيخ‎,šayḫ,elder
+```
+
+These "variations" are accomplished in a structured way. Indic retroflex consonants are added with a dot on the tailless side. Arabic sounds are added with a dot on the tailed side usually. Clicks are mapped to the closest mouth-position-oriented sound. Ejectives and implosives are simply added with accent marks. Etc.
+
+## Usage
+
+```bash
+npm install @mountbuild/hanakana
+```
+
+Then in JavaScript or Node.js land:
+
+```js
+const toHanakana = require('@mountbuild/hanakana')
+
+document.body.innerHTML = toHanakana('he+lo wrld')
+```
+
+Here is how you might use it more robustly:
+
+```js
+const lines = `fru+m ma+unt yu faind c+u+ stoun, ritxiq tu c+u+ klaud
+no we+r an rc du yu fil c+u+ pa+wr u+v netxr al u+ra+und
+wi+c c+u+ klaud so klos, yu ka+n si, tu+tx, a+nd test
+fidiq yor e+nrdji tu c+u+ stoun wi+c hest
+ma+unt holdz i+t xep, laik c+u+ sikri+t bal`
+  .split(/\n+/)
+  .map(line => line.split(/\s+/).map(toHanakana).join(' '))
+  .join('\n')
+
+document.body.innerHTML = lines.split(/\n/)
+  .map(line => `<div>${line}</div>`)
+  .join('\n')
+```
+
+## Dictionaries
+
+Take note of the `dictionary` folder in this project, which has the beginnings of words in several languages with their pronunciation written in ASCII Hanákana (which can be directly transformed into Hanákana script using this library). Also take note of the `languages` folder, which has transformers for common language paradigms (like Chinese Pinyin) into Hanákana script. This way you can take a word written in a certain way using a certain formality, and get the Hanákana out of it.
+
+## Symbols
+
+<p align="center">
+  <img src="https://github.com/mountbuild/hanakana.js/blob/build/symbols.png?raw=true" />
+</p>
+
+<h3 id="license">License</h3>
+
+Copyright 2021 <a href='https://mount.build'>Mount</a>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+<h3 id="mount">Mount</h3>
+
+Hanakana.js is being developed by the folks at [Mount](https://mount.build), a California-based project for helping humanity master information and computation. Mount started off in the winter of 2008 as a spark of an idea, to forming a company 10 years later in the winter of 2018, to a seed of a project just beginning its development phases. Mount funds hanakana.js's development. It is entirely bootstrapped by working full time and running [Etsy](https://etsy.com/shop/mountbuild) and [Amazon](https://www.amazon.com/s?rh=p_27%3AMount+Build) shops. Also find us on [Facebook](https://www.facebook.com/mountbuild), [Twitter](https://twitter.com/mountbuild), and [LinkedIn](https://www.linkedin.com/company/mountbuild). Check out our other GitHub projects as well!
+
+<br/>
+<br/>
+
+<p align='center'>
+  <a href='https://twitter.com/mountbuild'>
+    <img src='https://mount.build/slate/twitter.png' height='64' />
+  </a>　　　　
+  <a href='https://etsy.com/shop/mountbuild'>
+    <img src='https://mount.build/slate/etsy.png' height='64' />
+  </a>　　　　
+  <a href='https://github.com/mountbuild'>
+    <img src='https://mount.build/slate/github.png' height='64' />
+  </a>　　　
+  <a href='https://www.amazon.com/s?rh=p_27%3AMount+Build'>
+    <img src='https://mount.build/slate/amazon.png' height='64' />
+  </a>　　　
+  <a href='https://www.facebook.com/mountbuild'>
+    <img src='https://mount.build/slate/facebook.png' height='64' />
+  </a>
+</p>
+
+<br/>
+<br/>
