42480e2661664812e318dc04986bd0c4c86e6b2c
Lance Pollard <lp@elk.fm>
2022-10-29 20:50:12 -0700

    make

--- test.js
+++ test.js
@@ -1,9 +1,13 @@
 
 const tone = require('.')
+const words = require('../tone.land/seeds/words.json')
 
-test('a#&++*-_^b')
-test('a#&-*++_^b')
+// test('a#&++*-_^b')
+// test('a#&-*++_^b')
 test('ton')
+test('wa++i--ma++i--yuA++n')
+
+words.forEach(word => test(word.pronunciation))
 
 function test(a) {
   console.log(a, tone(a))
