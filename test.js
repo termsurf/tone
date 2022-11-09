3ccdd564f868855bff1a4ec5caeca4432e214b59
Lance Pollard <lp@elk.fm>
2022-11-08 21:10:59 -0800

    update

--- test.js
+++ test.js
@@ -4,10 +4,12 @@ const words = require('../tone.land/seeds/words.json')
 
 // test('a#&++*-_^b')
 // test('a#&-*++_^b')
-test('ton')
-test('wa++i--ma++i--yuA++n')
+// test('ton')
+// test('wa++i--ma++i--yuA++n')
 
 words.forEach(word => test(word.pronunciation))
+
+test('byu^dy')
 
 function test(a) {
   console.log(a, tone(a))
